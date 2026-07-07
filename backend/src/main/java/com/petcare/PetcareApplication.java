71c1840529ca63af99663682bfdbc7aae413cbcf
Xuân Trường <alonesera1612@gmail.com>
2026-07-07 06:30:11 +0000

    first commit

---
+++ backend/src/main/java/com/petcare/PetcareApplication.java
@@ -0,0 +1,11 @@
+package com.petcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PetcareApplication.class, args);
+    }
+}
