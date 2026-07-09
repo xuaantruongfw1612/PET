6dd5a2578ae46710a01736494fdcced14c86f6d0
xuaantruongfw1612 <alonesera1612@gmail.com>
2026-07-09 21:45:57 +0700

    controller, model, repository product

---
+++ backend/src/main/java/com/petcare/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.petcare.repository;
+
+import com.petcare.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+    boolean existsByName(String name);
+}
